301ce15473c8a46078ef5835a3674aa300eed5d2
soumik652 <soumik652@hotmail.com>
2016-07-25 20:34:46 +0530

    Initial Commit

--- src/main/java/com/websystique/springmvc/dao/StudentDao.java
+++ src/main/java/com/websystique/springmvc/dao/StudentDao.java
@@ -2,10 +2,16 @@ package com.websystique.springmvc.dao;
 
 import com.websystique.springmvc.model.Student;
 
+import java.util.List;
+
 /**
  * Created by omoto on 25/7/16.
  */
 public interface StudentDao {
 
     Student findById(int id);
+
+    void save(Student student);
+
+    List<Student> findAllUsers();
 }
